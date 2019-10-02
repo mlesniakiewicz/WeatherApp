74ca5a0c0024fd09412ebe308009bb04c90ea828
Michal L <michal.lesniakiewicz@gmail.com>
2019-10-02 14:02:31 +0200

    add

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ class App extends Component {
 
   handleCitySubmit = e => {
     e.preventDefault();
-    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
 
     fetch(API)
       .then(response => {
