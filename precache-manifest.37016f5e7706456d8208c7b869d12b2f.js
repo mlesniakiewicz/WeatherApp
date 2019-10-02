21d856181553250bdf4e3fae99abedd1245c9c02
Michal L <michal.lesniakiewicz@gmail.com>
2019-10-02 13:52:42 +0200

    Updates

---
+++ precache-manifest.37016f5e7706456d8208c7b869d12b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "2385acb50d2ccb23318a",
+    "url": "/WeatherApp/static/js/main.2385acb5.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/WeatherApp/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "2385acb50d2ccb23318a",
+    "url": "/WeatherApp/static/css/main.67e4f77d.chunk.css"
+  },
+  {
+    "revision": "c962f2f32853bbf642488181979987da",
+    "url": "/WeatherApp/index.html"
+  }
+];
